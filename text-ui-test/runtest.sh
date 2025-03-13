61c7d422bb46a9a765ab680cf543ad2e098fbe77
kinneo <kinneo2002@gmail.com>
2025-03-13 14:49:10 +0800

    Update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -8,16 +8,17 @@ cd ..
 
 cd text-ui-test
 
-#java  -jar $(find ../build/libs/ -mindepth 1 -print -quit) < input.txt > ACTUAL.TXT
-#
-#cp EXPECTED.TXT EXPECTED-UNIX.TXT
-#dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
-#diff EXPECTED-UNIX.TXT ACTUAL.TXT
-#if [ $? -eq 0 ]
-#then
-#    echo "Test passed!"
-#    exit 0
-#else
-#    echo "Test failed!"
-#    exit 1
-#fi
+
+# Ensure correct JAR file is used
+java -jar ../build/libs/healthbud.jar < input.txt > ACTUAL.TXT
+
+cp EXPECTED.TXT EXPECTED-UNIX.TXT
+dos2unix EXPECTED-UNIX.TXT ACTUAL.TXT
+diff EXPECTED-UNIX.TXT ACTUAL.TXT
+if [ $? -eq 0 ]; then
+    echo "Test passed!"
+    exit 0
+else
+    echo "Test failed!"
+    exit 1
+fi
