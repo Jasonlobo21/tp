f6049412f4f2b11488c0ebf88b78ca1d00ac85ae
kinneo <kinneo2002@gmail.com>
2025-03-18 21:09:43 +0800

    fixed indentation for workout exception class

--- src/main/java/seedu/healthbud/exception/InvalidWorkoutException.java
+++ src/main/java/seedu/healthbud/exception/InvalidWorkoutException.java
@@ -1,7 +1,7 @@
 package seedu.healthbud.exception;
 
 public class InvalidWorkoutException extends RuntimeException {
-  public InvalidWorkoutException() {
-    super("Invalid add command (add <date> /e <exercise> /r <reps> /s <sets>)");
-  }
+    public InvalidWorkoutException() {
+        super("Invalid add command (add <date> /e <exercise> /r <reps> /s <sets>)");
+    }
 }
