91aabbab6578959f1125bd4375829f94f20fefab
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-03-19 01:43:33 +0800

    Coding standards

--- src/main/java/seedu/healthbud/command/Command.java
+++ src/main/java/seedu/healthbud/command/Command.java
@@ -3,5 +3,6 @@ package seedu.healthbud.command;
 import seedu.healthbud.LogList;
 
 public abstract class Command {
-    public abstract void execute(LogList mealLogs, LogList workoutLogs, LogList waterLogs, String input) throws Exception;
+    public abstract void execute(LogList mealLogs, LogList workoutLogs, LogList waterLogs, String input)
+            throws Exception;
 }
--- src/main/java/seedu/healthbud/log/Water.java
+++ src/main/java/seedu/healthbud/log/Water.java
@@ -13,15 +13,19 @@ public class Water extends Log {
         this.time = time;
     }
 
-    public String getAmount() { return amount; }
+    public String getAmount() {
+        return amount;
+    }
 
-    public String getGlasses() { return glasses; }
+    public String getGlasses() {
+        return glasses;
+    }
 
-    public String getTime() { return time; }
-
+    public String getTime() {
+        return time;
+    }
 
     public String toString() {
         return String.format("%s %s glasses, %s ml on %s at %s", getName(), glasses, amount, getDate(), time);
     }
 }
-
