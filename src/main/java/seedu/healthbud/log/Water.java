5a6c52658d216b07afb69f0f81f462b2002d879d
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-03-20 11:41:27 +0800

    Coding Standards

--- src/main/java/seedu/healthbud/log/Water.java
+++ src/main/java/seedu/healthbud/log/Water.java
@@ -4,13 +4,17 @@ public class Water extends Log {
 
     private String amount;
     private String time;
-    private static int glass = 250;
-    private static int bottle = 1000;
+    private static int glass;
+    private static int bottle;
 
     public Water(String name, String amount, String date, String time){
         super(name, date);
         this.amount = amount;
         this.time = time;
+        updateVariables(amount);
+    }
+
+    public void updateVariables(String amount) {
         int amountInt = Integer.parseInt(amount);
         this.glass = amountInt/250;
         this.bottle = amountInt/1000;
