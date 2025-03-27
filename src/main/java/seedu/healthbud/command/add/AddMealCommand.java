7c8b4c94e1978e7eda99a45b81e19395636b826c
AHMISH\naira <nair.ahmish@gmail.com>
2025-03-27 16:50:53 +0800

    update oop

--- src/main/java/seedu/healthbud/command/add/AddMealCommand.java
+++ src/main/java/seedu/healthbud/command/add/AddMealCommand.java
@@ -4,22 +4,22 @@ import seedu.healthbud.LogList;
 import java.util.Map;
 import seedu.healthbud.ParserParameters;
 import seedu.healthbud.command.Command;
-import seedu.healthbud.exception.InvalidMealException;
+import seedu.healthbud.exception.InvalidLogException;
 import seedu.healthbud.log.Meal;
 
-public abstract class AddMealCommand extends Command {
+public class AddMealCommand extends Command {
 
-    public void execute(LogList mealLogs, String input) throws InvalidMealException {
+    public void execute(LogList mealLogs, String input) throws InvalidLogException {
         String[] parts = input.trim().split(" ");
         String command = parts[1];
         if (parts.length < 2) {
-            throw new InvalidMealException();
+            throw new InvalidLogException();
         }
 
         Map<String, String> param = ParserParameters.parseParameters(input);
 
         if (!input.contains("/cal ") || !input.contains("/d ") || !input.contains("/t ")) {
-            throw new InvalidMealException();
+            throw new InvalidLogException();
         }
 
         input = input.substring("add meal".length()).trim();
@@ -28,14 +28,20 @@ public abstract class AddMealCommand extends Command {
 
         if (param.get("cal").isEmpty() || param.get("d").isEmpty() || param.get("t").isEmpty()
                 || mealName.isEmpty()) {
-            throw new InvalidMealException();
+            throw new InvalidLogException();
         }
 
         if (!param.get("cal").matches("\\d+")) {
-            throw new InvalidMealException();
+            throw new InvalidLogException();
         }
 
         Meal newMeal = new Meal(mealName, param.get("cal"), param.get("d"), param.get("t"));
         mealLogs.addLog(newMeal);
+    }
+
+    @Override
+    public void execute(LogList goalLogs, LogList pbLogs, LogList mealLogs, LogList workoutLogs,
+                        LogList waterLogs, LogList cardioLogs, String input) throws Exception {
+
     }
 }
--- src/main/java/seedu/healthbud/command/add/AddWorkoutCommand.java
+++ src/main/java/seedu/healthbud/command/add/AddWorkoutCommand.java
@@ -4,17 +4,17 @@ import seedu.healthbud.LogList;
 import java.util.Map;
 import seedu.healthbud.ParserParameters;
 import seedu.healthbud.command.Command;
-import seedu.healthbud.exception.InvalidWorkoutException;
+import seedu.healthbud.exception.InvalidLogException;
 import seedu.healthbud.log.WorkOUT;
 
-public abstract class AddWorkoutCommand extends Command {
+public class AddWorkoutCommand extends Command {
 
 
-    public void execute(LogList workoutLogs, String input) throws InvalidWorkoutException {
+    public void execute(LogList workoutLogs, String input) throws InvalidLogException {
         String[] parts = input.trim().split(" ");
         String command = parts[1];
         if (parts.length < 2) {
-            throw new InvalidWorkoutException();
+            throw new InvalidLogException();
         }
 
         Map<String, String> param = ParserParameters.parseParameters(input);
@@ -22,7 +22,7 @@ public abstract class AddWorkoutCommand extends Command {
         assert input != null : "Invalid workout input!";
 
         if (!input.contains("/r ") || !input.contains("/s ") || !input.contains("/d ")) {
-            throw new InvalidWorkoutException();
+            throw new InvalidLogException();
         }
 
         input = input.substring("add workout".length()).trim();
@@ -31,14 +31,20 @@ public abstract class AddWorkoutCommand extends Command {
 
         if (param.get("r").isEmpty() || param.get("s").isEmpty() || param.get("d").isEmpty()
                 || workoutName.isEmpty()) {
-            throw new InvalidWorkoutException();
+            throw new InvalidLogException();
         }
 
         if (!param.get("r").matches("\\d+") || !param.get("s").matches("\\d+")) {
-            throw new InvalidWorkoutException();
+            throw new InvalidLogException();
         }
 
         WorkOUT newWorkout = new WorkOUT(workoutName, param.get("r"), param.get("s"), param.get("d"));
         workoutLogs.addLog(newWorkout);
+    }
+
+    @Override
+    public void execute(LogList goalLogs, LogList pbLogs, LogList mealLogs, LogList workoutLogs,
+                        LogList waterLogs, LogList cardioLogs, String input) throws Exception {
+
     }
 }
