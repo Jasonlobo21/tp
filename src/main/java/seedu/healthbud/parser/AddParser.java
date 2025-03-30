bfcc78ce66e6b06c2033b44213b5890032fa4209
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-03-31 02:30:48 +0800

    add bug fix

--- src/main/java/seedu/healthbud/parser/AddParser.java
+++ src/main/java/seedu/healthbud/parser/AddParser.java
@@ -14,6 +14,7 @@ import seedu.healthbud.exception.InvalidGoalException;
 
 public class AddParser {
 
+    
     public static Command parse(String subCommand, LogList mealLogs, LogList waterLogs, LogList cardioLogs,
                                 LogList pbLogs, LogList workoutLogs, LogList goalLogs, String input) throws InvalidLogException,
                                 InvalidCardioException, InvalidMealException, InvalidPBException, InvalidWaterException,
