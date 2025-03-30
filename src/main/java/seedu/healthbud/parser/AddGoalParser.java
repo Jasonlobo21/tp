0e71be9829e7da6ca0ffc51cc6235c393737854f
keanesworkspace-admin <ngyewsiong@gmail.com>
2025-03-31 00:41:56 +0800

    add goal print

--- src/main/java/seedu/healthbud/parser/AddGoalParser.java
+++ src/main/java/seedu/healthbud/parser/AddGoalParser.java
@@ -9,6 +9,7 @@ import seedu.healthbud.log.Goals;
 import java.util.Map;
 
 public class AddGoalParser{
+    static Goals goal = Goals.getInstance();
 
     private static String addMissingParameters(String input){
         if (!input.contains("/cal")){
@@ -30,7 +31,7 @@ public class AddGoalParser{
             throw new InvalidGoalException();
         }
         if (!input.contains("/w") && !input.contains("/cal") && !input.contains("/kg")) {
-            Ui.printMessage("Welcome to goal setting! Here are your current goals: \n");
+            Ui.printMessage("Welcome to goal setting! Here are your current goals: \n" + Goals.getInstance().toString());
             Ui.printMessage("What goal would you like to add today?\n");
             return new AddGoalCommand(goalLogs, input, Goals.getInstance().getDailyWaterGoal(),
                     Goals.getInstance().getDailyCalorieGoal(), Goals.getInstance().getWeightGoal());
