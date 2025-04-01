b04da5f1735e3f9a73c805f3de27db17b45dd35b
kinneo <kinneo2002@gmail.com>
2025-04-01 19:32:36 +0800

    HOT FIX

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -12,9 +12,13 @@ for /f "tokens=*" %%a in (
     set jarloc=%%a
 )
 
-java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TXT
-
+rem -- Force Java to run with UTF-8 encoding
 cd ..\..\text-ui-test
+chcp 65001 > nul
+java -Dfile.encoding=UTF-8 -jar ..\build\libs\%jarloc% < input.txt > ACTUAL.TXT
 
 FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
+
+
+
 
