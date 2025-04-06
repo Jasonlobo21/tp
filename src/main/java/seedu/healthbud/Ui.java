6e7b7b83378fefa8ae207238c3e074162326eddd
kinneo <kinneo2002@gmail.com>
2025-04-07 01:57:39 +0800

    XAKNXKASIECNIOLWSEHJOIW

--- src/main/java/seedu/healthbud/Ui.java
+++ src/main/java/seedu/healthbud/Ui.java
@@ -26,13 +26,13 @@ public class Ui {
     /**
      * Prints the HealthBud ASCII art to the user.
      */
-    public static void printHealthBuds() {
-        System.out.println(" _   _            _ _   _     ____            _");
-        System.out.println("| | | | ___  __ _| | |_| |__ | __ ) _   _  __| |___");
-        System.out.println("| |_| |/ _ \\/ _` | | __| '_ \\|  _ \\| | | |/ _` / __|");
-        System.out.println("|  _  |  __/ (_| | | |_| | | | |_) | |_| | (_| \\__ \\");
-        System.out.println("|_| |_|\\___|\\__,_|_|\\__|_| |_|____/ \\__,_|\\__,_|___/");
-    }
+//    public static void printHealthBuds() {
+//        System.out.println(" _   _            _ _   _     ____            _");
+//        System.out.println("| | | | ___  __ _| | |_| |__ | __ ) _   _  __| |___");
+//        System.out.println("| |_| |/ _ \\/ _` | | __| '_ \\|  _ \\| | | |/ _` / __|");
+//        System.out.println("|  _  |  __/ (_| | | |_| | | | |_) | |_| | (_| \\__ \\");
+//        System.out.println("|_| |_|\\___|\\__,_|_|\\__|_| |_|____/ \\__,_|\\__,_|___/");
+//    }
 
     /**
      * Prints the greeting message to the user.
@@ -41,7 +41,7 @@ public class Ui {
      * </p>
      */
     public static void printGreeting() {
-        printHealthBuds();
+        //printHealthBuds();
         printMessage(NEW_LINE + "Hello! I'm HealthBud" + NEW_LINE + "What can I do for you?");
     }
 
