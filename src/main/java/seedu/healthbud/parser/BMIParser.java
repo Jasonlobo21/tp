1fa50788fbf7b22f32d419c451a0c23d96049d4b
AHMISH\naira <nair.ahmish@gmail.com>
2025-04-08 04:45:09 +0800

    hot fix

--- src/main/java/seedu/healthbud/parser/BMIParser.java
+++ src/main/java/seedu/healthbud/parser/BMIParser.java
@@ -44,7 +44,7 @@ public class BMIParser {
         double weight = Double.parseDouble(param.get("w"));
         double height = Double.parseDouble(param.get("h"));
 
-        if(height >= 3.0 || height <= 0.2 || weight <= 1.5 || weight >= 700) {
+        if(height > 3.0 || height < 0.2 || weight < 1.5 || weight > 700) {
             throw new HealthBudException(
                     "Weight should be between 1.5kg and 700kg and height should be between 0.2m and 3.0m.");
         }
